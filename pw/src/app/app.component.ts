3c40e04a9516ebcc95e28b9224b5f58c3ddcd0f5
Cristian Buffa <bufface@gmail.com>
2020-03-15 10:25:56 -0300

    017 Generating a Random Password

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -33,12 +33,26 @@ export class AppComponent {
   }
   
   onButtonClick() {
-    console.log(`
-      Letters: ${this.includeLetters}
-      Numbers: ${this.includeNumbers}
-      Symbols: ${this.includeSymbols}
-    `);
-    
-    this.password = 'MY SUPER PASSWORD!!!'
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
