9dfa7d8e8dfa202d2340c3f691876afa2860f536
Cristian Buffa <bufface@gmail.com>
2020-03-22 09:59:51 -0300

    066 Getting Some Data

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60'
+    }
+  ]
 }
