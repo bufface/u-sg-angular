f96af5cf2f31033ec4a983b9030f3cf1db03eb8a
Cristian Buffa <bufface@gmail.com>
2020-03-22 09:35:28 -0300

    062 Custom Arguments

--- pipes/src/app/convert.pipe.ts
+++ pipes/src/app/convert.pipe.ts
@@ -5,12 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
+  transform(value: any, targetUnits: string): any {
     if (!value) {
       return ''
     }
 
-    return value * 1.60934
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934
+      case 'm':
+        return value * 1.60934 * 1000
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000
+      default:
+        throw new Error('Target unit not supported')
+    }
+
+    return 
   }
 
 }
