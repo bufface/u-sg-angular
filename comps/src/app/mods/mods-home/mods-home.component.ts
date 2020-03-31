91f3e14a9657fc07b7d032ba3d30ddf797658f00
Cristian Buffa <bufface@gmail.com>
2020-03-31 11:39:19 -0300

    145 Listing Accordion Elements

--- comps/src/app/mods/mods-home/mods-home.component.ts
+++ comps/src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false
+  items = [
+    { title: 'Some random title', content: 'Some random text content'},
+    { title: 'Some random title 1', content: 'Some random text content 1'},
+    { title: 'Some random title 2', content: 'Some random text content 2'}
+  ]
 
   constructor() { }
 
