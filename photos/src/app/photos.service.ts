4cbd2bcb98c05a13aaa5414e2ae796fb94019fb9
Cristian Buffa <bufface@gmail.com>
2020-04-17 10:18:17 -0300

    214 Making the Request

--- photos/src/app/photos.service.ts
+++ photos/src/app/photos.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID ezi5jZ7-GDVjlpQ8okyYMq9PkpAXawgDscKptY1ybbY'
+      }
+    })
+  }
 }
