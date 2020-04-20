024bd6e7235db6f1283bc824439aaed1ef43b4fe
Cristian Buffa <bufface@gmail.com>
2020-04-20 10:50:45 -0300

    237 Reformatting the Input

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { DateFormControl } from '../date-form-control'
 
 @Component({
   selector: 'app-card-form',
@@ -10,7 +11,7 @@ export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [ Validators.required, Validators.minLength(3) ]),
     cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
+    expiration: new DateFormControl(''),
     securityCode: new FormControl('')
   })
 
