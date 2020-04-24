33b058a4aaca1ed93e3715556343f5d710531d66
Cristian Buffa <bufface@gmail.com>
2020-04-24 10:17:33 -0300

    253 Displaying Form Values

--- mathform/src/app/equiation/equiation.component.ts
+++ mathform/src/app/equiation/equiation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-equiation',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equiation.component.css']
 })
 export class EquiationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('')
+  })
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get a () {
+    return this.mathForm.value.a
+  }
+
+  get b () {
+    return this.mathForm.value.b
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10)
+  }
 }
