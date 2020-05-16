eb7077f103e4dc2e96dc0756b76a60b9d4480f02
Cristian Buffa <bufface@gmail.com>
2020-05-16 10:59:49 -0300

    278 Implementing Async Validators

--- emailclient/src/app/auth/validators/match-password.ts
+++ emailclient/src/app/auth/validators/match-password.ts
@@ -1,7 +1,9 @@
 import { Validator, FormGroup } from '@angular/forms'
 import { Injectable } from '@angular/core'
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class MatchPassword implements Validator {
   validate(formGroup: FormGroup) {
     const { password, confirmation } = formGroup.value
